bcf56eb22a3d87c417a07950e160f03e1afdcd7f
jiazhigang <jiazhigang_888@163.com>
2025-04-25 16:30:46 +0800

    first commit

---
+++ src/pages/unocss/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="text-6xl color-pink font-semibold">
+    Hello, Unocss!
+  </h1>
+
+  <p class="text-gray-700 mt-10 dark:text-white">
+    This is a simple example of Unocss in action.
+  </p>
+
+  <button class="mt-10 btn">
+    Button
+  </button>
+</template>
+
+<route lang="json5">
+{
+  name: 'unocss',
+  meta: {
+    title: '🎨 Unocss 示例',
+    i18n: 'menus.unocssExample'
+  },
+}
+</route>
