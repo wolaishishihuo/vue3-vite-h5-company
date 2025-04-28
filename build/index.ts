b782578b6ae862fad63b443b4383b65bdf33e5e0
wolaishishihuo <2430250298@qq.com>
2025-04-28 18:14:12 +0800

    feat: 添加@vitejs/plugin-vue-jsx插件支持

--- build/index.ts
+++ build/index.ts
@@ -4,11 +4,14 @@ import UnoCSS from 'unocss/vite';
 import AutoImport from 'unplugin-auto-import/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
 import Components from 'unplugin-vue-components/vite';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
 import { createViteVConsole } from './vconsole';
 
 export function createVitePlugins(mode: string) {
   return [
     vue(),
+    vueJsx(),
 
     // https://github.com/antfu/unplugin-vue-components
     Components({
