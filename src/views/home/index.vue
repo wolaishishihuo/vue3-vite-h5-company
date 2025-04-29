6bc95cfeeaf184cb91598c3b4d43443c932c8e90
jiazhigang <jiazhigang_888@163.com>
2025-04-29 09:00:52 +0800

    fix: 修复弹窗组件错误处理及样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="flex flex-col gap-[30px]">
     <Search />
-    <!-- <van-cell title="URL 跳转" is-link :inset="true" @click="showPopup" /> -->
     <van-cell title="选择器" is-link :inset="true" @click="showPickerPopup" />
-    <!-- <van-cell title="日期选择" is-link :inset="true" @click="showDatePickerPopup" /> -->
     <van-button round block type="primary" native-type="submit">
       提交
     </van-button>
-    <!-- <van-picker
+    <van-picker
       title="标题"
       :columns="columns"
-    /> -->
+    />
   </div>
 </template>
 
 <script setup lang='tsx'>
-import { PopupManager } from '@/composables/usePopup';
-import { ref } from 'vue';
 import { Picker } from 'vant';
-import 'vant/es/picker/style';
+import { PopupManager } from '@/composables/usePopup';
 
 defineOptions({
   name: 'Home'
@@ -31,17 +27,6 @@ const columns = ref([
   { text: '绍兴', value: 'Shaoxing' },
   { text: '湖州', value: 'Huzhou' }
 ]);
-
-// const showPopup = () => {
-//   PopupManager.open({
-//     content: '这是一个弹窗示例',
-//     position: 'bottom',
-//     closeable: true,
-//     closeIcon: 'cross',
-//     closeOnClickOverlay: true,
-//     teleport: 'body'
-//   });
-// };
 
 const showPickerPopup = () => {
   const popup = PopupManager.open({
@@ -59,26 +44,4 @@ const showPickerPopup = () => {
     )
   });
 };
-
-// const showDatePickerPopup = () => {
-//   const popup = PopupManager.open({
-//     title: '选择日期',
-//     position: 'bottom',
-//     closeable: true,
-//     closeIcon: 'cross',
-//     closeOnClickOverlay: true,
-//     teleport: 'body',
-//     content: () => {
-//       const DatePicker = defineAsyncComponent(() => import('vant/es/date-picker'));
-//       return h(DatePicker, {
-//         type: 'date',
-//         onConfirm: (value: any) => {
-//           console.log('选择的日期:', value);
-//           popup.close();
-//         },
-//         onCancel: () => popup.close()
-//       });
-//     }
-//   });
-// };
 </script>
