948622120a7663218b3588bb7c474c2e778cf8e1
wolaishishihuo <2430250298@qq.com>
2025-05-07 11:24:50 +0800

    style: 更新按钮样式及添加阴影效果

--- uno.config.ts
+++ uno.config.ts
@@ -12,8 +12,9 @@ import {
 const BASE_FONT_SIZE = 8;
 
 export default defineConfig({
+  // 自定义类样式
   shortcuts: [
-    ['btn', 'px-6 py-3 rounded-3 border-none inline-block bg-green-400 text-white cursor-pointer outline-hidden hover:bg-green-600 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50']
+    ['btn', 'px-6 py-3 rounded-[50px] border-none inline-block bg-[#3875C6] text-white cursor-pointer outline-hidden hover:bg-[#2d5d9e] disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50 shadow-[0px_21px_38px_0px_rgba(56,117,198,0.32)]']
   ],
   presets: [
     presetWind4({
