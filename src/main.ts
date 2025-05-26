f0dd9f1fbfe23e4847d2c7e8d8b3d1c5d9b15c4d
wolaishishihuo <2430250298@qq.com>
2025-05-26 14:44:26 +0800

    feat: 添加权限指令及重构应用初始化逻辑

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,20 @@ import pinia from '@/stores';
 import 'virtual:uno.css';
 import '@/styles/app.less';
 import '@/styles/var.less';
-import '@/styles/vant.less';
+import '@/styles/reset.less';
 import '@/assets/iconfont/iconfont.css';
+import directives from '@/directives/index';
+import registerPlugins from '@/plugins/index';
+import { Lazyload } from 'vant';
 // Vant 桌面端适配
 import '@vant/touch-emulator';
-import registerPlugins from './plugins';
-
 // 创建Vue应用实例
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
+app.use(Lazyload);
+app.use(directives);
 
 // router 准备就绪后挂载应用
 router.isReady().then(() => {
