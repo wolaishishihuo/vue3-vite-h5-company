94a731ca973814ea61830e1be5f4b7cfa3ebd860
wolaishishihuo <2430250298@qq.com>
2025-05-30 13:58:10 +0800

    feat: 新增人员组织页面及相关功能

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="p-20px flex-col gap-20px">
+    <h1 class="text-center">
+      Demo 示例
+    </h1>
+
     <template v-for="item in list" :key="item.path">
       <Cell :title="item.title" :path="item.path" />
     </template>
@@ -21,6 +25,18 @@ const list = [
   {
     title: '定位按钮',
     path: '/demo/locationButton'
+  },
+  {
+    title: '地图定位',
+    path: '/demo/mapLocation'
+  },
+  {
+    title: '表格滚动列表',
+    path: '/demo/tableScrollList'
+  },
+  {
+    title: '人员组织',
+    path: '/demo/organization'
   }
 ];
 </script>
