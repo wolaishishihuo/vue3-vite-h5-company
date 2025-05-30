78aae768bf79b6a42bdc60889a29b9e7d6cd5b97
jiazhigang <jiazhigang_888@163.com>
2025-05-30 11:49:02 +0800

    feat: 优化应用结构，新增导航栏和标签栏

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <router-view v-slot="{ Component, route }">
-    <div class="app-wrapper bg-white">
-      <transition
-        appear name="fade-transform" mode="out-in" @after-enter="onTransitionFinished"
-        @after-leave="onTransitionFinished"
-      >
+  <div class="flex-col h-full">
+    <nav-bar />
+    <div class="flex-1 overflow-auto">
+      <router-view v-slot="{ Component, route }">
         <keep-alive :include="routeCaches">
           <component :is="createComponentWrapper(Component, route)" :key="route.fullPath" />
         </keep-alive>
-      </transition>
+      </router-view>
     </div>
-  </router-view>
+    <tab-bar />
+  </div>
 </template>
 
 <script setup lang="ts">
 import useRouteCacheStore from '@/stores/modules/routeCache';
-import { useRouteTransition } from '@/composables/useRouteTransition';
 import { storeToRefs } from 'pinia';
 
 const routeCacheStore = useRouteCacheStore();
 const { routeCaches } = storeToRefs(routeCacheStore);
-
-const { completeTransition } = useRouteTransition();
-const onTransitionFinished = () => {
-  completeTransition();
-};
 
 const wrapperMap = new Map();
 function createComponentWrapper(component, route) {
@@ -38,32 +31,3 @@ function createComponentWrapper(component, route) {
   return h(wrapper);
 }
 </script>
-
-<style lang="less" scoped>
-.app-wrapper {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: hidden;
-  > div {
-    height: 100%;
-  }
-}
-/* 页面切换动画 */
-.fade-transform-enter-active,
-.fade-transform-leave-active {
-  transition: all 0.5s;
-}
-
-.fade-transform-enter-from {
-  opacity: 0;
-  transform: translateX(-0.3rem);
-}
-
-.fade-transform-leave-to {
-  opacity: 0;
-  transform: translateX(0.3rem);
-}
-</style>
