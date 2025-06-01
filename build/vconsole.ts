ea191e1e1f3101808ef0697a5d5d03bb55a03eb0
jiazhigang <jiazhigang_888@163.com>
2025-06-01 20:43:57 +0800

    fix: 修复vconsole启用条件判断逻辑

--- build/vconsole.ts
+++ build/vconsole.ts
@@ -4,7 +4,7 @@ import { viteVConsole } from 'vite-plugin-vconsole';
 export const createViteVConsole = (viteEnv: ViteEnv) => {
   return viteVConsole({
     entry: [path.resolve('src/main.ts')],
-    enabled: viteEnv.VITE_APP_VCONSOLE === 'true',
+    enabled: String(viteEnv.VITE_APP_VCONSOLE) === 'true',
     config: {
       maxLogNumber: 1000,
       theme: 'light'
